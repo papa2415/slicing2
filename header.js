c454a1413096df1a33d86c962b65130a6d4d9c80
papa2415 <167390811+papa2415@users.noreply.github.com>
2024-06-01 22:08:32 +0800

    first-edition

---
+++ header.js
@@ -0,0 +1,17 @@
+$(".navbar-btn").click(function () {
+  $(".header-list").toggleClass("active");
+  $(".non_mask").toggleClass("mask");
+});
+$(".header-link").hover(
+  function () {
+    $(this).append(
+      $(`<span><div class="border b-lt"></div>
+    <div class="border b-rt"></div>
+    <div class="border b-lb"></div>
+    <div class="border b-rb"></div></span>`)
+    );
+  },
+  function () {
+    $(this).find("span").last().remove();
+  }
+);
